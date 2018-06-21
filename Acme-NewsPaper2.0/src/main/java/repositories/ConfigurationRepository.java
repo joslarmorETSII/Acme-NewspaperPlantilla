a470faedf78f0f3e26523ec6625c1d673e53e468
Unknown <joslarmor@alum.us.es>
2018-06-21 20:22:59 +0200

    f

--- Acme-NewsPaper2.0/src/main/java/repositories/ConfigurationRepository.java
+++
@@ -1,9 +0,0 @@
-package repositories;
-
-import domain.Configuration;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ConfigurationRepository extends JpaRepository<Configuration,Integer> {
-}
--- Acme-NewsPaper2.0/src/main/java/repositories/UserAccountRepository.java
+++
@@ -1,9 +0,0 @@
-package repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import security.UserAccount;
-
-@Repository
-public interface UserAccountRepository extends JpaRepository<UserAccount, Integer> {
-}
